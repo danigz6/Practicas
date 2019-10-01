b23acde846f67cbd6564d49c52dd1825ec033af4
danigz6 <dani.gaza6@users.noreply.github.com>
2019-10-01 10:43:24 +0200

    clase teorica, bucles anidados

---
+++ triangulorectAnidados.c
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+
+int main() {
+    int i, j, num;
+
+    cout<<"Introduce ancho: ";
+    cin>>num;
+
+    for (i=0; i<num; i++){
+        for(j=0; j<=i; j++){
+            cout<<"*";
+            cout<<endl;
+        }
+    }
+
+    return 0;
+}

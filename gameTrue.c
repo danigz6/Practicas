3e26a2e0054c44fffe8b0c0007da8087a5177dbb
danigz6 <dani.gaza6@users.noreply.github.com>
2019-10-01 10:32:45 +0200

    terminar en casa

---
+++ gameTrue.c
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+
+int main() {
+    int random,num,intents=0;
+    bool win=false;
+
+    srand(time(NULL));
+    random=rand()%20+1;
+
+    while (!win && intents<=7){
+
+    }
+
+    return 0;
+}
